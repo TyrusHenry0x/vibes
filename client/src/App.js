832cc939b073a8076d590e567aa71e8d735df4de
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-10-23 11:31:05 -0400

    navbar (not functional)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <div className="header">
+        <h2>Vibes</h2>
+      </div>
+
+      <div className="navbar">
+        <a href="#" className="nav-link">
+          <span className="nav-button">Home</span>
+        </a>
+        <a href="#" className="nav-link">
+          <span className="nav-button">New Vibe</span>
+        </a>
+        <a href="#" className="nav-link">
+          <span className="nav-button">About</span>
+        </a>
+        <a href="#" className="nav-link">
+          <span className="nav-button">Contact Me</span>
+        </a>
+      </div>
 
     </div>
   );
