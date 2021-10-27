e39af157d2a9ffe24487ea4cb4a2153714bf94fa
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-10-27 14:59:51 -0400

    t

---
+++ src/components/EditFunction.js
@@ -0,0 +1,9 @@
+import { Route } from "react-router-dom"
+
+const EditFunction = () => {
+
+  return (
+
+    <Route path="/editvibe"></Route>
+  )
+}
--- src/components/Vibes.jsx
+++ src/components/Vibes.jsx
@@ -1,12 +1,19 @@
 import axios from "axios"
+import editf
 
-export const Vibes = (props) => {
+export const Vibes = (props, toggleFetch, setToggleFetch) => {
 
+  const deleteVibe = async () => {
+    await axios.delete(`https://api.airtable.com/v0/apppfkjVKyYXyDqYM/Table%201?api_key=key5SMVCWBp7tBUcr/${props.vibe.id}`)
+    setToggleFetch(!toggleFetch)
+  }
 
   return (
     <div className="post">
       <h1><b>{props.vibe.fields.Name}</b></h1>
       <p>{props.vibe.fields.text}</p>
+      <Link to='/editvibes'>Edit</Link>
+      <button onClick={deleteVibe}>Delete</button>
     </div>
   )
 }
