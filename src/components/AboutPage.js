8582bb19170cba0ab71c2a23a7f26d1a91a62003
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-10-28 10:59:23 -0400

    about

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,12 @@
+import { Link } from "@material-ui/core"
 
 const AboutPage = () => {
 
   return (
-    <div>
+    <div className="about">
       <h1>About</h1>
       <p>Vibes is a platform that focuses on positivity in day to day life, it could be something as simple as your breakfast tasting great, or finding the bright side of a bad situation. </p>
+      <Link to="/" exact><button>Home</button></Link>
     </div>
   )
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import { Link } from "@material-ui/core"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Contact = () => {
+
+  return (
+    <div>
+      <a href="https://github.com/TyrusHenry0x/"><GitHubIcon /></a>
+      <a href="https://www.linkedin.com/in/tyrus-henry-bb409021a/"><LinkedInIcon /></a>
+    </div>
+  )
+}
