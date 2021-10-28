91ee0926494a8377f2c370a9ca5c36f58523dec1
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-10-28 14:33:55 -0400

    links out working

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
 
-      <div className="header"><h1 id="vibes">Vibes</h1></div>
+      {/* <div className="header"><h1 id="vibes">Vibes</h1></div> */}
 
       <div className="bottom">
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
@@ -6,13 +5,16 @@ const Contact = () => {
 
   return (
     <div>
-      <Link href="https://github.com/TyrusHenry0x/">
+      <a href="https://github.com/TyrusHenry0x/">
         <GitHubIcon />
-      </Link>
+      </a>
+
       <a href="https://www.linkedin.com/in/tyrus-henry-bb409021a/">
-        linkedin
         <LinkedInIcon />
       </a>
+
+      <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
+      </p>
     </div>
   )
 }
