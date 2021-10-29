4272080c83e9a9b60af7e78e72ee3805c7bd0b3c
tyrus <tyrus@adminisatorsMBP.attlocal.net>
2021-10-29 03:06:46 -0400

    about page refined

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,7 +4,6 @@ const AboutPage = () => {
     <div className="about">
       <h1>About</h1>
       <img src="https://www.pngall.com/wp-content/uploads/5/Sea-Wave-Transparent.png" className="wave" alt="wave"></img>
-
       <p id="about-text">Vibes is a platform that focuses on positivity in day to day life, it could be something as simple as your breakfast tasting great, or finding the bright side of a bad situation. </p>
     </div>
   )
